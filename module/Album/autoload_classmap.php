5717984529132f5c1f59d60b05690b29cff4880f
huytv <huyhuong>
2015-03-24 02:07:55 +0700

    install zend album

---
+++ module/Album/autoload_classmap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 3/24/15
+ * Time: 1:51 AM
+ */
+return array();
---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 3/24/15
+ * Time: 1:57 AM
+ */
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
